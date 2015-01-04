a13308b4bbcc92d4fb7c099421a1befa3beee2f8
Antonin <antonin.morvan@gmail.com>
2015-01-04 21:31:56 +0100

    Add num touches

--- app/js/main.js
+++ app/js/main.js
@@ -12,11 +12,13 @@ var keyboard = function(mode,langue,os){
         PAD :
             {
               _default : { 1 : 'A', 2 : 'Z', 3 : 'E', 4 : 'R', 5 : 'T', 6 : 'Y', 7 : 'U', 8 : 'I', 9 : 'O', 10 : 'P', 11 : 'Q', 12 : 'S', 13 : 'D',14 : 'F',15 : 'G',16 : 'H',17 : 'J',18 : 'K',19 : 'L',20 : 'M',21 : 'W',22 : 'X',23 : 'C',24 : 'V',25 : 'B',26 : 'N', cancel : '', enter : '', 29 : '123' },
-              _shift : { 1:'F1',2 : 'F2',3 : 'F3',4 : 'F4',5 : 'F5',6 : 'F6',7 : 'F7', 8 : 'F8', 9 : 'F9' }
+              _shift : { 1 : 'A', 2 : 'Z', 3 : 'E', 4 : 'R', 5 : 'T', 6 : 'Y', 7 : 'U', 8 : 'I', 9 : 'O', 10 : 'P', 11 : 'Q', 12 : 'S', 13 : 'D',14 : 'F',15 : 'G',16 : 'H',17 : 'J',18 : 'K',19 : 'L',20 : 'M',21 : 'W',22 : 'X',23 : 'C',24 : 'V',25 : 'B',26 : 'N', cancel : '', enter : '', 29 : '123' },
+              _alt : {}
             },
         NUM :
             {   _default : { 1 : '1', 2 : '2', 3 : '3', 4 : '4', 5 : '5', 6 : '6', 7 : '7', 8 : '8', 9 : '9', 10 : '0', 11 : '@', 12 : '#', 13 : '$', 14 : '%', 15 : '&', 16 : '*', 17 : '-', 18 : '+', 19 : '(', 20 : ')', 21 : '!', 22 : '"', 23 : '"', 24 : ':', 25 : ';', 26 : '/', cancel : '', enter : '', 29 : 'ABC' },
-                _shift : { 1:'F1', 2 : 'F2', 3 : 'F3', 4 : 'F4', 5 : 'F5', 6 : 'F6', 7 : 'F7', 8 : 'F8', 9 : 'F9' }
+                _shift : { 1:'F1', 2 : 'F2', 3 : 'F3', 4 : 'F4', 5 : 'F5', 6 : 'F6', 7 : 'F7', 8 : 'F8', 9 : 'F9',10 : 'F10', 11 :  'F11', 12 : 'F12', 13 : '?', 14 : '`', 15 : '|', 16  : '_',17 : '~', 18 : '^',19 : '|', 20 : '[', 21 : ']', 22 : '{', 23 : '}', 24 : '<-', 25 : 'bas', 26 : '->', cancel : '', enter : '', 29 : 'ABC' },
+                _alt : { 1:'F1', 2 : 'F2', 3 : 'F3', 4 : 'F4', 5 : 'F5', 6 : 'F6', 7 : 'F7', 8 : 'F8', 9 : 'F9',10 : 'F10', 11 :  'F11', 12 : 'F12', 13 : '?', 14 : '`', 15 : '|', 16  : '_',17 : '~', 18 : '^',19 : '|', 20 : '[', 21 : ']', 22 : '{', 23 : '}', 24 : '<-', 25 : 'bas', 26 : '->', cancel : '', enter : '', 29 : 'ABC' }
             }
         }
 
@@ -24,6 +26,7 @@ var keyboard = function(mode,langue,os){
     this.addControlsTouch = function(){
         var container = document.getElementById('control'),
         touch = this.controls,
+        os = this.os,
         el = this;
 
         for (var id in touch) {
@@ -31,7 +34,7 @@ var keyboard = function(mode,langue,os){
             li.className = 'col-mb-1';
 
             span = document.createElement('span');
-            id == 'cmd' ? span.className = 'icon icon_mac_'+id : span.className = 'icon icon_'+id;
+            id == 'cmd' ? span.className = 'icon icon_'+os+'_'+id : span.className = 'icon icon_'+id;
             txt = document.createTextNode(touch[id]);
 
             id == 'space' ? li.className = 'col-mb-4' : '';
